40eab9acbf2dcdbe81d62253781c0f8e742b3ec7
Mario Raafat <mariorafat10@gmail.com>
2024-08-23 04:20:37 +0300

    task3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- two columns ordered by 
+SELECT 
+    band_name, 
+    (IFNULL(split, 2020) - formed) AS lifespan
+FROM
+    metal_bands
+WHERE 
+    style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
